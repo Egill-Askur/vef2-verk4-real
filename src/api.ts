cf6834dcb484eaef22b66899a3fcc0c4e17709ab
Egill-Askur <eae28@hi.is>
2025-03-20 20:46:08 +0000

    fixed another lint issue

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-import { Category, Paginated, Question } from './types';
+import { Category, Paginated } from './types';
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL ?? 'http://localhost:8000';
 
